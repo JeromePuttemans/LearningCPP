dfcd1f1eb802f8e43dae8d9df3048475ae79dd9e
Jerome Puttemans <Jerome.puttemans@gmail.com>
2025-08-17 13:45:41 +0200

    Mastering C++ Game Animation Programming

---
+++ Mastering C++ Game Animation Programming/Mastering-Cpp-Game-Animation-Programming-main/chapter02/01_opengl_computeshader/opengl/ShaderStorageBuffer.cpp
@@ -0,0 +1,33 @@
+#include "ShaderStorageBuffer.h"
+
+void ShaderStorageBuffer::init(size_t bufferSize) {
+  mBufferSize = bufferSize;
+
+  glGenBuffers(1, &mShaderStorageBuffer);
+
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, mShaderStorageBuffer);
+  glBufferData(GL_SHADER_STORAGE_BUFFER, mBufferSize, nullptr, GL_DYNAMIC_COPY);
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+}
+
+void ShaderStorageBuffer::bind(int bindingPoint) {
+  if (mBufferSize == 0) {
+    return;
+  }
+
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, mShaderStorageBuffer);
+  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, bindingPoint, mShaderStorageBuffer);
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+}
+
+void ShaderStorageBuffer::checkForResize(size_t newBufferSize) {
+  if (newBufferSize > mBufferSize) {
+    Logger::log(1, "%s: resizing SSBO %i from %i to %i bytes\n", __FUNCTION__, mShaderStorageBuffer, mBufferSize, newBufferSize);
+    cleanup();
+    init(newBufferSize);
+  }
+}
+
+void ShaderStorageBuffer::cleanup() {
+  glDeleteBuffers(1, &mShaderStorageBuffer);
+}
